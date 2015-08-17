c1921c137704c52850664cae995604052b69847d
James Duff <jaoduff@gmail.com>
2015-08-17 21:11:37 +0800

    Gave up on promises for the moment. All tests so far passing with callbacks. Implemented reading data from .json files in data directory

--- quiz.js
+++ quiz.js
@@ -1,5 +1,39 @@
 module.exports = function Quiz(){
-  this.listFiles = function(){
+  var promisify, readfile, fs, datadir;
+  promisify = require("es6-promisify"); //https://www.npmjs.com/package/es6-promisify
+  fs = require('fs')
+  readdir = promisify(fs.readdir)
+  readfile = promisify(fs.readFile)
+  datadir = "./data"
 
-  }
+this.listFiles = function(callback){
+  fs.readdir(datadir, readfiles)
+}
+
+function readfiles(err, list){
+  if (err)throw err;
+    var reg = /\.json$/i
+    var JSONList = list.filter(function(i){
+      if (reg.test(i)){return i}
+    })
+    console.log(JSONList)
+    callback(JSONList)
+}
+
+/*
+  this.listFiles = function(){
+    return new Promise(function (resolve, reject){
+      readdir(datadir).map(function(filename){
+          var reg = /\.json$/i
+          var JSONList = list.filter(function(i){
+            if (reg.test(i)){return readfile(datadir + "/" + filename)}
+          })
+        })
+      var list = ["chlorine", 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+      resolve(list)
+    })
+    .then(function(list){
+      console.log(list)
+    })
+  }*/
 }
--- test/quiz.js
+++ test/quiz.js
@@ -14,8 +14,65 @@ describe('quiz class', function(){
 
   describe('listFiles', function(){
     var quiz = new Quiz("elements")
-    it('should eventually return a list', function(){
-      quiz.listFiles().should.eventually.be.a('list')
+    it('should eventually return an array', function(){
+      //return quiz.listFiles().should.eventually.be.a('array')
+      quiz.listFiles(function(err, list){
+        if (err) throw err;
+        list.should.be.a('array')
+      })
+    })
+    it('should eventually return an array with a size greater than 0', function(){
+      //return quiz.listFiles().should.eventually.have.length.above(0)
+      quiz.listFiles(function(err, list){
+        if (err) throw err;
+        list.should.have.length.above(0)
+      })
+    })
+    it('should eventually return an array containing "chlorine"', function(){
+      //return quiz.listFiles().should.eventually.contain("chlorine")
+      quiz.listFiles(function(err, list){
+        if (err) throw err;
+        list.should.contain("chlorine")
+      })
+    })
+    describe('listFiles("elements")', function(){
+      it('should eventually return an array with a length of 25', function(){
+        //return quiz.listFiles().should.eventually.have.length.of(25)
+        quiz.listFiles(function(err, list){
+          if (err) throw err;
+          list.should.have.length.of(25)
+        })
+      })
+      it('should eventually return an array with all the elements', function(){
+        quiz.listFiles(function(err, list){
+          if (err) throw err;
+            list.should.contain("aluminium")
+            list.should.contain("chlorine")
+            list.should.contain("barium")
+            list.should.contain("bromine")
+            list.should.contain("calcium")
+            list.should.contain("chromium")
+            list.should.contain("cobalt")
+            list.should.contain("copper")
+            list.should.contain("fluorine")
+            list.should.contain("gold")
+            list.should.contain("hydrogen")
+            list.should.contain("iron")
+            list.should.contain("lead")
+            list.should.contain("magnesium")
+            list.should.contain("mercury")
+            list.should.contain("nickel")
+            list.should.contain("nitrogen")
+            list.should.contain("oxygen")
+            list.should.contain("phosphorus")
+            list.should.contain("potassium")
+            list.should.contain("silver")
+            list.should.contain("sodium")
+            list.should.contain("sulfur")
+            list.should.contain("tin")
+            list.should.contain("zinc")
+          })
+      })
     })
   })
 
