29af7038ec61d65c7d71baf908b0666741ce5bdf
James Duff <jaoduff@gmail.com>
2015-10-17 14:00:33 +0800

    angular routing

--- public/application.js
+++ public/application.js
@@ -2,6 +2,12 @@ var mainApplicationModuleName = 'school_quiz';
 
 var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
 
+mainApplicationModule.config(['$locationProvider',
+	function($locationProvider) {
+		$locationProvider.hashPrefix('!');
+	}
+]);
+
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainApplicationModuleName]);
 });
