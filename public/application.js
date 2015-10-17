bae8146d432e29093fd0c8fba32a6b73128d072e
James Duff <jaduff@graduate.uwa.edu.au>
2015-10-17 08:34:57 +0800

    working on Angular controllers

--- public/application.js
+++ public/application.js
@@ -1,6 +1,6 @@
 var mainApplicationModuleName = 'school_quiz';
 
-var mainApplicationModule = angular.module(mainApplicationModuleName, ['example']);
+var mainApplicationModule = angular.module(mainApplicationModuleName, ['ngRoute', 'example']);
 
 angular.element(document).ready(function() {
 	angular.bootstrap(document, [mainApplicationModuleName]);
